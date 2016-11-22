58e8bbb6c0929a2067687c29fd19aa7ac281e167
Adarsh-MR <adarshmr1998@gmail.com>
2016-11-22 12:21:21 +0000

    Add CAF Branch to Build prop
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- citrus.mk
+++ citrus.mk
@@ -41,3 +41,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_FINGERPRINT=Xiaomi/kenzo/kenzo:6.0.1/MMB29M/6.7.21:user/release-keys \
     PRIVATE_BUILD_DESC="kenzo-user 6.0.1 MMB29M 6.7.21 release-keys"
 endif
+
+PRODUCT_PROPERTY_OVERRIDES += \
+ 		ro.caf.branch=LA.BR.1.3.6_rb1.8
