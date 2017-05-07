4f9772482f038dd405b6ac4f5a684f07385a43da
Adarsh-MR <adarshmr1998@gmail.com>
2017-05-07 08:53:33 +0000

    Citrus-fy kenzo
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,10 +30,10 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 26838785024 # 26838801408 - 16384
 
 # Kernel
-TARGET_KERNEL_CONFIG := lineageos_kenzo_defconfig
+TARGET_KERNEL_CONFIG := citrus_kenzo_defconfig
 
 # Properties
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_SYSTEM_PROP +=  device/xiaomi/kenzo/system.prop
 
 # Sepolicy
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
